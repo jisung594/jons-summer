0fd6fc67c3daa479fa0906e7c794f2282bc44df1
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-08-02 15:05:58 -0500

    Adds homepage rough draft text

--- src/App.js
+++ src/App.js
@@ -5,21 +5,36 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
 
         <h3> :) HELLO FIRST FUNEMPLOYMENT PROJECT :) </h3>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hi, my name is Jon. My Korean name is Jisung,
+          and it was the name I grew up using until I was 17
+          when I got my US citizenship.
+        </p>
+
+        <p>
+          Due to federal budgets cuts earlier in the year,
+          the company I worked at for about 4 years was affected
+          and I was laid off.
+        </p>
+
+        <p>
+          Surpisingly enough, I've been wholeheartedly enjoying
+          my funemployment. I've been learning to bake loaves of bread and
+          restore (basic) furniture. I've been tending to my patio garden
+          and taking my dog on longer walks.
+        </p>
+
+        <p>
+          This [event] did give me a clearer headspace to re-evaluate my personal goals,
+          including those as a web developer. I've always looked to work my background in
+          graphic design into my dev work where I can, so I decided to build a mini component library
+          as a fun personal project / portfolio piece. I used Figma to design my components 
+          and Storybook to build and style them.
+
+        </p>
+
       </header>
     </div>
   );
