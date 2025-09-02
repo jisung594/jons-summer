a6e739c47a9b9f05e21143267a1ab1cbc9085b00
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-09-02 15:13:44 -0500

    Adds Header component

---
+++ src/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './Header.module.css';
+
+export const Header = ({
+  isMiniUiKitPage
+}) => {
+
+  const headerClass = isMiniUiKitPage ? styles.headerLight : styles.headerDark;
+  
+  return (
+    <div className={`${styles.header} ${headerClass}`}>
+      <nav>
+        <NavLink to="/" className={styles.navLink} activeClassName={styles.activeLink}>HOME</NavLink>
+        <NavLink to="/mini-ui-kit" className={styles.navLink} activeClassName={styles.activeLink}>MINI UI KIT</NavLink>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
