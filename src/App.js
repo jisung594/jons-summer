6112be6d23429d87a4eb0d4c85390a8bfb14a359
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2025-09-02 15:18:52 -0500

    Restructures main content / routing

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { Bio } from './Bio/Bio.jsx';
+import { MiniUiKit } from './MiniUiKit/MiniUiKit.jsx';
+import { Header } from './Header/Header.jsx';
+import { Footer } from './Footer/Footer.jsx';
 import styles from './App.module.css';
 
-function App() {
+
+const AppContent = () => {
+  // Get the current location object
+  const location = useLocation();
+  // Check if the current pathname is '/mini-ui-kit'
+  const isMiniUiKitPage = location.pathname === '/mini-ui-kit';
+
   return (
-    <div className={styles.App}>
-      <header className={styles.homepageHeader}>
+    <div className="App">
+      {/* Pass the boolean prop to the Header component */}
+      <Header isMiniUiKitPage={isMiniUiKitPage} />
 
-        <h1> header placeholder</h1>
-
-      </header>
-
-
-      {/* <h3 className={styles.homepageTitle}> :) HELLO FIRST FUNEMPLOYMENT PROJECT :) </h3> */}
-      <h3 className={styles.homepageTitle}>Hi, my name is Jon.</h3>
-
-
-
-      <Bio />
-
-      <div className={styles.loafImages}>
-        <img src="/images/boule_1.jpg" alt="loaf of bread" />
-        <img src="/images/boule_2.jpg" alt="loaf of bread" />
-      </div>
-
+      <Routes>
+        <Route path="/" element={<Bio />} />
+        <Route path="/mini-ui-kit" element={<MiniUiKit />} />
+      </Routes>
+      
+      <Footer />
     </div>
+  );
+};
+
+
+function App() {
+  // // const [currentPage, setCurrentPage] = useState('home');
+      
+  // // Get the current location object
+  // const location = useLocation();
+  // // Check if the current pathname is '/mini-ui-kit'
+  // const isMiniUiKitPage = location.pathname === '/mini-ui-kit';
+
+  return (
+      <BrowserRouter>
+          <AppContent />
+      </BrowserRouter>
   );
 }
 
