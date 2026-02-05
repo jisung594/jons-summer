aac798c5952d2004e68eea81851896d70d94dcf7
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2026-02-05 14:44:43 -0600

    Update footer styling

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import styles from './Footer.module.css';
 
-export const Footer = ({
-  ...props
-}) => {
+export const Footer = ({}) => {
   return (
     <div className={styles.footer}>
-      <div className={styles.footerContent}>
-        <div className={styles.socialMediaWrapper}>
+      <div className={styles.footerMain}>
+        <div className={styles.contactInfoSection}>
+          <label>CONTACT</label>
+          <div className={styles.contactInfo}>
+            <a href="mailto:your.email@example.com">jonjichoi@gmail.com</a>
+            <a href='tel:+19786044087'>(978) 604-4087</a>
+          </div>
+        </div>
+
+        <div className={styles.socialMediaSection}>
           <a href="https://www.linkedin.com/in/jonathanchoi594">
             <img src={`${process.env.PUBLIC_URL}/images/social/linkedin.png`} alt={'LinkedIn icon'} />
           </a>
@@ -15,16 +21,9 @@ export const Footer = ({
             <img src={`${process.env.PUBLIC_URL}/images/social/github.png`} alt={'Github icon'} />
           </a>
         </div>
-        <div className={styles.contactInfoWrapper}>
-          <label>CONTACT</label>
-          <div className={styles.contactInfo}>
-            <a href="mailto:your.email@example.com">jonjichoi@gmail.com</a>
-            <a href='tel:+19786044087'>978.604.4087</a>
-          </div>
-        </div>
       </div>
-      <div className={styles.copyrightWrapper}>
-        <p>&copy; 2025 Jonathan Choi. All Rights Reserved.</p>
+      <div className={styles.footerCopyright}>
+        <p className={styles.footerCopyrightText}>&copy; 2026 Jonathan Choi. All Rights Reserved.</p>
       </div>
     </div>
   );
