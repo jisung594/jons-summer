98262d90974b13839f29430b484814f9c909d683
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2026-02-06 06:33:56 -0600

    About component edits

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -6,29 +6,36 @@ export const About = ({ ...props }) => {
       <div className={styles.experience}>
         <h1 className={styles.experienceTitle}>Professional Experience</h1>
         <ul className={styles.experienceList}>
-          <li>React</li>
-          <li>JavaScript</li>
-          <li>Drupal</li>
-          <li>PHP</li>
-          <li>HTML/CSS</li>
-          <li>Cypress</li>
-          <li>Figma</li>
-          <li>Storybook</li>
+          <div className={styles.experienceListCol}>
+            <li>React</li>
+            <li>JavaScript</li>
+            <li>HTML/CSS</li>
+          </div>
+          <div className={styles.experienceListCol}>
+            <li>Drupal</li>
+            <li>PHP</li>
+            <li>Twig</li>
+          </div>
+          <div className={styles.experienceListCol}>
+            <li>Behat</li>
+            <li>Cypress</li>
+          </div>
+          <div className={styles.experienceListCol}>
+            <li>Figma</li>
+            <li>Storybook</li>
+          </div>
         </ul>
       </div>
       <div className={styles.tools}>
         <h1 className={styles.toolsTitle}>Current Tools</h1>
         <ul className={styles.toolsList}>
-          <li>Angular</li>
-          <li>Typescript</li>
-          <li>Tailwind CSS</li>
-          <li>Firebase</li>
+          <div className={styles.experienceListCol}>
+            <li>Angular</li>
+            <li>Typescript</li>
+            <li>Tailwind CSS</li>
+            <li>Firebase</li>
+          </div>
         </ul>
-        {/* <div className={styles.content}>
-          <p>
-            lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet
-          </p>
-        </div> */}
       </div>
 
     </div>
