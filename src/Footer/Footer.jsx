189775d7ec59fe2619eb6c929dc6015022dd884f
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2026-02-06 10:47:49 -0600

    cleanup

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Footer.module.css';
 
-export const Footer = ({}) => {
+export const Footer = () => {
   return (
     <div className={styles.footer}>
       <div className={styles.footerMain}>
