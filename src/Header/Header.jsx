7921ae48c16884a7e9c42091ae842cf7dfa4b94a
Jonathan Choi <jonathanchoi@Jonathans-MacBook-Pro.local>
2026-02-06 11:25:43 -0600

    Tweak header shadow

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { useLocation, NavLink } from 'react-router-dom';
 import styles from './Header.module.css';
 
 export const Header = ({
   isMiniUiKitPage
 }) => {
-  const headerClass = isMiniUiKitPage ? styles.headerLight : styles.headerDark;
-  
+  const location = useLocation();
+  const headerShadowClass = location.pathname === '/2025' 
+    ? styles.headerDarkShadow
+    : styles.headerLightShadow;
+
   return (
-    <div className={`${styles.header} ${headerClass}`}>
+    <div className={`${styles.header} ${headerShadowClass}`}>
       <nav>
         <NavLink 
           to="/" 
-          className={styles.navLink} 
+          className={styles.navLink}
           activeClassName={styles.activeLink}
         >HOME</NavLink>
         <NavLink 
           to="/about" 
-          className={styles.navLink} 
+          className={styles.navLink}
           activeClassName={styles.activeLink}
         >ABOUT</NavLink>
         <NavLink 
           to="/2025" 
-          className={styles.navLink} 
+          className={styles.navLink}
           activeClassName={styles.activeLink}
         >2025</NavLink>
       </nav>
